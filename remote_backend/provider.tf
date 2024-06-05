3be981f4ca628b76afa66001f851563630e64691
devprabhudev <146797951+devprabhudev@users.noreply.github.com>
2024-06-05 20:48:23 +0530

    Update provider.tf

--- remote_backend/provider.tf
+++ remote_backend/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "remotetfbucket "
+    bucket = "remotetfbucket"
     region = "eu-north-1"
     dynamodb_table = "remote_backend_table"
     key = "dev/dev-terraform.tfstate"
